cd32d3f07c32569da1a90b4ef89c7b377b94aa7a
Soulyvanh Phetsarath <soulyvap@metropolia.fi>
2020-11-19 18:31:58 +0200

    Implemented 3-dot menu with setup item

--- app/src/main/java/com/example/mealtrackerapp/MainActivity.java
+++ app/src/main/java/com/example/mealtrackerapp/MainActivity.java
@@ -1,11 +1,13 @@
 package com.example.mealtrackerapp;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
-import android.widget.Button;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -13,13 +15,22 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
 
-        findViewById(R.id.btnSetup).setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.menuItemSetup:
                 Intent intent = new Intent(MainActivity.this, Setup.class);
                 startActivity(intent);
-            }
-        });
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
