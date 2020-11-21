51ef60f1438d4aac99c17ac462ccdcbe41882d70
Soulyvanh Phetsarath <soulyvap@metropolia.fi>
2020-11-21 22:47:21 +0200

    Added spinner for meal selection in food entry activity

--- app/src/main/java/com/example/mealtrackerapp/FoodEntryActivity.java
+++ app/src/main/java/com/example/mealtrackerapp/FoodEntryActivity.java
@@ -6,25 +6,60 @@ import android.app.TimePickerDialog;
 import android.content.Context;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageButton;
 import android.widget.ImageView;
+import android.widget.Spinner;
 import android.widget.TimePicker;
+import android.widget.Toast;
 
+import java.util.ArrayList;
 import java.util.Calendar;
 
-public class FoodEntryActivity extends AppCompatActivity {
+public class FoodEntryActivity extends AppCompatActivity implements AdapterView.OnItemSelectedListener {
 
-    EditText timeDisplay;
+    EditText timeDisplay, foodName, foodCalories, foodCarbs, foodProtein, foodFat;
+    Spinner mealSpinner;
     ImageView btnClock;
     int hour, minutes;
+    String mealSelected;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_food_entry);
 
+        //meal selection with spinner
+        mealSpinner = findViewById(R.id.spinnerMeal);
+
+        //creating arrayList with meal choice
+        ArrayList<String> meals = new ArrayList<>();
+        meals.add("Choose meal ...");
+        meals.add("Breakfast");
+        meals.add("Lunch");
+        meals.add("Dinner");
+        meals.add("Extras");
+
+        //setting up arrayAdapter for spinner with elements of the arrayList and attaching adapter to spinner object
+        ArrayAdapter<String> mealSpinnerAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_item, meals);
+        mealSpinnerAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        mealSpinner.setAdapter(mealSpinnerAdapter);
+
+        //set click listener
+        mealSpinner.setOnItemSelectedListener(this);
+
+//        mealSpinner.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+//            @Override
+//            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+//
+//            }
+//        });
+
+
+        //time selection with time picker dialog
         timeDisplay = findViewById(R.id.etxtTime);
 
         Calendar calendar = Calendar.getInstance();
@@ -57,4 +92,15 @@ public class FoodEntryActivity extends AppCompatActivity {
         }
     }
 
+    @Override
+    public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+        //storing meal item selected to a string
+        mealSelected = parent.getItemAtPosition(position).toString();
+        Toast.makeText(this, mealSelected, Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    public void onNothingSelected(AdapterView<?> parent) {
+
+    }
 }
