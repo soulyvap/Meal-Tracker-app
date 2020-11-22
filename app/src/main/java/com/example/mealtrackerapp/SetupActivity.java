bec92f38058679e10686e68186318b3c458bf5a7
nehar <neharana@metropolia.fi>
2020-11-23 03:04:27 +1400

    setup date picker dailog box

--- app/src/main/java/com/example/mealtrackerapp/SetupActivity.java
+++ app/src/main/java/com/example/mealtrackerapp/SetupActivity.java
@@ -3,16 +3,22 @@ package com.example.mealtrackerapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.app.Activity;
+import android.app.DatePickerDialog;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.DatePicker;
 import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.TextView;
 import android.widget.Toast;
 
-public class SetupActivity extends AppCompatActivity {
+import java.util.Calendar;
+
+public class SetupActivity extends AppCompatActivity implements View.OnClickListener {
     public static final String PREF_CALORIC_GOAL = "extra_caloricGoal";
     public static final String PREF_CARBS = "extra_carbs";
     public static final String PREF_PROTEIN = "extra_protein";
@@ -22,11 +28,20 @@ public class SetupActivity extends AppCompatActivity {
     EditText firstName, LastName, height, weight, caloricGoal, carbsPercent, proteinPercent,
             fatPercent, waterGoal;
     Button btnCalendar, btnSave;
+    ImageView btnBirthdateCalendar;
+
+    private int mYear, mMonth, mDay, mHour, mMinute;
+    TextView txtBirthdate;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_setup);
+
+        btnBirthdateCalendar = findViewById(R.id.btnBirthdateCalendar);
+        txtBirthdate = findViewById(R.id.txtBirthdate);
+        btnBirthdateCalendar.setOnClickListener(this);
 
         btnSave = findViewById(R.id.btnSaveSetup);
         btnSave.setOnClickListener(new View.OnClickListener() {
@@ -59,5 +74,39 @@ public class SetupActivity extends AppCompatActivity {
 
             }
         });
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.btnBirthdateCalendar:
+                ShowDateDialog();
+                break;
+        }
+    }
+
+    private void ShowDateDialog() {
+
+        // Get Current Date
+        final Calendar c = Calendar.getInstance();
+        mYear = c.get(Calendar.YEAR);
+        mMonth = c.get(Calendar.MONTH);
+        mDay = c.get(Calendar.DAY_OF_MONTH);
+
+
+        DatePickerDialog datePickerDialog = new DatePickerDialog(this,
+                new DatePickerDialog.OnDateSetListener() {
+
+                    @Override
+                    public void onDateSet(DatePicker view, int year,
+                                          int monthOfYear, int dayOfMonth) {
+
+                        txtBirthdate.setText(dayOfMonth + "-" + (monthOfYear + 1) + "-" + year);
+
+                    }
+                }, mYear, mMonth, mDay);
+        datePickerDialog.show();
+
+
     }
 }
