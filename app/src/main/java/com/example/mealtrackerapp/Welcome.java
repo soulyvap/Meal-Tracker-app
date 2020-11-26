0e85d3217c87d4c033b072f046c6e392a1ccde4b
nehar <neharana@metropolia.fi>
2020-11-26 15:57:57 +0530

    activity_welcome.xml created button

--- app/src/main/java/com/example/mealtrackerapp/Welcome.java
+++ app/src/main/java/com/example/mealtrackerapp/Welcome.java
@@ -2,15 +2,24 @@ package com.example.mealtrackerapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
+import android.view.View;
+import android.widget.Button;
 
 public class Welcome extends AppCompatActivity {
-
+    Button buttonSetup;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_welcome);
-
+    buttonSetup=findViewById(R.id.button);
+    buttonSetup.setOnClickListener(new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            startActivity(new Intent(Welcome.this,SetupActivity.class));
+        }
+    });
     }
 }
