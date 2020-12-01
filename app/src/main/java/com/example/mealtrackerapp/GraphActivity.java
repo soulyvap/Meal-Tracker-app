ba659de6b4382ae974e2cd8f8a8ac558f4fa2994
nehar <neharana@metropolia.fi>
2020-12-01 16:42:52 +0530

    put some codes

--- app/src/main/java/com/example/mealtrackerapp/GraphActivity.java
+++ app/src/main/java/com/example/mealtrackerapp/GraphActivity.java
@@ -2,10 +2,14 @@ package com.example.mealtrackerapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.graphics.Color;
 import android.os.Bundle;
 
 import com.github.mikephil.charting.charts.BarChart;
+import com.github.mikephil.charting.data.BarData;
+import com.github.mikephil.charting.data.BarDataSet;
 import com.github.mikephil.charting.data.BarEntry;
+import com.github.mikephil.charting.utils.ColorTemplate;
 
 import java.util.ArrayList;
 import java.util.Random;
@@ -13,13 +17,38 @@ import java.util.Random;
 public class GraphActivity extends AppCompatActivity {
 
     BarChart barChart;
+    BarData barData;
+    BarDataSet barDataSet;
+    ArrayList barEntries;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_graph);
+        barChart = (BarChart) findViewById(R.id.barChart);
 
-        barChart = (BarChart) findViewById(R.id.bargraph);
+        getEntries();
+        barDataSet = new BarDataSet(barEntries, "Data Set");
+        barData = new BarData(barDataSet);
+
+        barChart.setData(barData);
+
+        barDataSet.setColors(ColorTemplate.MATERIAL_COLORS);
+        barDataSet.setValueTextColor(Color.BLACK);
+        barDataSet.setValueTextSize(16f);
+
+    }
+
+    private void getEntries() {
+        barEntries = new ArrayList<>();
+        barEntries.add(new BarEntry(1f,2));
+        barEntries.add(new BarEntry(2f,2));
+        barEntries.add(new BarEntry(3f,2));
+        barEntries.add(new BarEntry(5f,2));
+        barEntries.add(new BarEntry(6f,2));
+        barEntries.add(new BarEntry(7f,2));
+
 
 
     }
+
 }
