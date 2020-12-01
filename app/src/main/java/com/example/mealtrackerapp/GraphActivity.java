2fe1a0d6e5cbe7dc27fb39b56d9ad9198d4373a6
nehar <neharana@metropolia.fi>
2020-12-01 13:46:08 +0530

    created GraphActivity

--- app/src/main/java/com/example/mealtrackerapp/GraphActivity.java
+++ app/src/main/java/com/example/mealtrackerapp/GraphActivity.java
@@ -4,11 +4,22 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import com.github.mikephil.charting.charts.BarChart;
+import com.github.mikephil.charting.data.BarEntry;
+
+import java.util.ArrayList;
+import java.util.Random;
+
 public class GraphActivity extends AppCompatActivity {
 
+    BarChart barChart;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_graph);
+
+        barChart = (BarChart) findViewById(R.id.bargraph);
+
+
     }
 }
